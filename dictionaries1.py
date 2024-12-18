15361fe9d1225ec2c389f30906bbf0bf7923bc91
houdinimain <houdgondal@icloud.com>
2024-12-18 18:58:40 +0000

    Add files via upload

---
+++ dictionaries1.py
@@ -0,0 +1,44 @@
+movies = {"pathaan":"Shah ruh Khan","tiger 1":"Salman Khan","Dunki":"Shah ruh Khan","Jawan":"Shah ruh Khan","sultan":"Salman Khan"}
+
+
+print(movies)
+print(type(movies))
+
+print(movies.keys())
+
+print(movies.values())
+
+for i in movies:
+    print(i)
+
+for h in movies:
+    print(h,movies[h])
+
+
+
+print("Dunki" in movies)
+
+print("tiger2" in movies)
+
+
+
+print("Dunki"  not in movies)
+
+print("tiger2" not in movies)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
